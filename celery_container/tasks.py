358cede56addd062bdf5f3e88f180f1a38201776
Agustín Cignetti <agustinc@eventbrite.com>
2019-05-30 15:39:08 -0300

    More examples

--- celery_container/tasks.py
+++ celery_container/tasks.py
@@ -1,3 +1,5 @@
+import time
+
 from celery import Celery
 
 
@@ -10,4 +12,16 @@ app = Celery(
 
 @app.task
 def add(x, y):
+    if isinstance(x, str) or isinstance(y, str):
+        return f'Received two strings {x} and {y}'
     return x + y
+
+
+@app.task(bind=True)
+def hello(self, a, b):
+    time.sleep(1)
+    self.update_state(state="PROGRESS", meta={'progress': 50})
+    time.sleep(2)
+    self.update_state(state="PROGRESS", meta={'progress': 90})
+    time.sleep(3)
+    return 'hello world: {}'.format(a + b)
